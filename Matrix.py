9d3554a56d849c200072baadbee41d1e44e36f0a
erzelenki <er@zelenki.net>
2020-05-05 11:25:23 +0300

    New version printing only snakes

--- Matrix.py
+++ Matrix.py
@@ -3,95 +3,77 @@
 import random
 import time
 import os
+import sys
 
-print("\u000c")
-snake = (
-'0', '232', '233', '233', '233', '233', '234', '22', '22', '22', '28', '34', '34', '34', '34', '34', '34', '34', '34',
-'34', '34', '231')
+sys.stdout.write(u"\u001b[2J")
+snake = ('231', '34', '34', '34', '34', '34', '34', '34', '34', '34', '34', '28', '22', '22', '22', '234', '233', '233',
+         '232', '0')
+width = os.get_terminal_size().columns-1
+height = os.get_terminal_size().lines
 matrix = []
-wide = os.get_terminal_size().columns // 2
-high = os.get_terminal_size().lines
 headpoints = []
-prn = ''
-
-for i in range(high):
-    matrix.append([])
-    for j in range(wide):
-        matrix[i].append(["\u001b[38;5;0m", chr(random.randint(12448, 12543))])
+snakes = []
 
 
-def change_chr(matrix):
-    '''
-    :param matrix:
-    :return: Возвращает новую матрицу с измененными символами
-    '''
-    for _ in range(random.randint(0, 500)):
-        newi = random.randint(0, high - 1)
-        newj = random.randint(0, wide - 1)
-        newchr = chr(random.randint(12448, 12543))
-        matrix[newi][newj][1] = newchr
-    return matrix
+def create_snake(snake):
+    full_snake = []
+    for i in range(len(snake) - 1):
+        full_snake.append(["\u001b[38;5;" + snake[i] + "m", chr(random.randint(12448, 12543))])
+    full_snake.append(["\u001b[38;5;34m", "  "])
+    return full_snake
 
 
-def new_head(headpoints, wide):
-    '''
-    :param headpoints:
-    :param wide:
-    :return: Генерирует голову новой струи
-    '''
+def issue_snakes(headpoints, snake, width):
     points = []
     for i in range(len(headpoints)):
-        points.append(headpoints[i][1])
+        if headpoints[i][0] - len(snake) > 5:
+            points.append(headpoints[i][1])
     for i in range(random.randint(0, 4)):
-        new_point = random.randint(0, wide - 1)
+        new_point = 2*random.randint(0, width//2)
         if new_point not in points:
-            headpoints.append([0, new_point])
+            headpoints.append([0, new_point, create_snake(snake)])
     return headpoints
 
 
-def colorized(matrix, headpoints, wide, high):
-    '''
-    :param matrix:
-    :param headpoints:
-    :param wide:
-    :param high:
-    :return: Возвращает матрицу с подсвечеными струйками
-    '''
-    for i, j in headpoints:
-        counter = 1
+def move_snakes(headpoints, snake):
+    ii=0
+    while ii < (len(headpoints)):
+        if headpoints[ii][0] >= height + len(snake):
+            del headpoints[ii]
+        headpoints[ii] = [headpoints[ii][0] + 1, headpoints[ii][1], headpoints[ii][2]]
+        ii+=1
+    return headpoints
 
-        while i >= 0 and counter < len(snake) and i < high:
-            matrix[i][j][0] = "\u001b[38;5;" + snake[0 - counter] + "m"
+
+def change_snake(headpoints, snake):
+    for x in range(len(headpoints)):
+        for _ in range(random.randint(0, 2)):
+            newchr = chr(random.randint(12448, 12543))
+            newpos = random.ranint(0, len(snake))
+            headpoints[x][2][newpos][1] = newchr
+    return headpoints
+
+
+def current_matrix(headpoints, height, snake):
+    for i, j, k in headpoints:
+        counter = 0
+        while i-counter >= 0 and counter < len(snake) and i < height:
+            matrix.append([i - counter, j, k[counter]])
             counter += 1
-            i -= 1
-        counter = i - high + 1
-        i = i
-        while i >= high and counter < len(snake) and i < high + len(snake):
-            matrix[i - counter][j][0] = "\u001b[38;5;" + snake[0 - counter - 1] + "m"
+        counter = i - height
+        while i >= height and counter < len(snake) and i < height + len(snake):
+            matrix.append([i - counter, j, k[counter]])
             counter += 1
     return matrix
 
 
 while True:
-    wide = os.get_terminal_size().columns // 2
-    high = os.get_terminal_size().lines
-
-    headpoints = new_head(headpoints, wide)
-    matrix = colorized(matrix, headpoints, wide, high)
-    # print("\u001b[2H")
-    # print("\u001b[2J")
-    prn = ''
-    for j in range(high):
-        prn = prn + '\n'
-        for i in range(wide):
-            prn = prn + matrix[j][i][0] + matrix[j][i][1]
-    print(prn)
-    time.sleep(0.13)
-    ii = 0
-    while ii < (len(headpoints)):
-        if headpoints[ii][0] < high + len(snake):
-            headpoints[ii][0] += 1
-            ii += 1
-        else:
-            del headpoints[ii]
-    matrix = change_chr(matrix)
+    width = os.get_terminal_size().columns-1
+    height = os.get_terminal_size().lines
+    headpoints = issue_snakes(headpoints, snake, width)
+    matrix = current_matrix(headpoints, height, snake)
+    headpoints = move_snakes(headpoints, snake)
+    time.sleep(0.06)
+    for i in matrix:
+        sys.stdout.write(u"\u001b["+str(i[0])+";"+str(i[1])+"H")
+        sys.stdout.write(i[2][0]+i[2][1])
